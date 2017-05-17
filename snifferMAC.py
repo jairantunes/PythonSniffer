97917214466364fc610c61f7a9734383a19a2796
Jair Antunes <jair.a.carvalho.jr@gmail.com>
2017-05-17 14:33:06 +0000

    first commit

---
+++ snifferMAC.py
@@ -0,0 +1,37 @@
+import socket
+from general import *
+from networking.ethernet import Ethernet
+from networking.ipv4 import IPv4
+from networking.icmp import ICMP
+from networking.tcp import TCP
+from networking.udp import UDP
+from networking.pcap import Pcap
+from networking.http import HTTP
+
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t   '
+DATA_TAB_2 = '\t\t   '
+DATA_TAB_3 = '\t\t\t   '
+DATA_TAB_4 = '\t\t\t\t   '
+
+
+def main():
+    pcap = Pcap('capture.pcap')
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65535)
+        pcap.write(raw_data)
+        eth = Ethernet(raw_data)
+
+        print(eth)
+        print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac, eth.proto))
+
+    pcap.close()
+
+
+main()
